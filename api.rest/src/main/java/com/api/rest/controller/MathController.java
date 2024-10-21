068ce4bd6d728fce5df10effc3065e95725b6f0e
maryson011 <maryson011@gmail.com>
2024-10-21 14:36:30 -0300

    feat: greeting endpoint mathController with method sum

---
+++ api.rest/src/main/java/com/api/rest/controller/MathController.java
@@ -0,0 +1,35 @@
+package com.api.rest.controller;
+
+import com.api.rest.exceptions.UnsupportedMathOperationException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+    @RequestMapping(value = "/sun/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double sun(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please send a numeric value!");
+        }
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        if (strNumber == null) return 0D;
+        String number = strNumber.replaceAll(",", ".");
+        if (isNumeric(number)) return Double.parseDouble(number);
+        return 0D;
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
