f48ebc62b70dceb5afd0348a7fba0eec39b8c7e1
maryson011 <maryson011@gmail.com>
2024-10-22 15:42:12 -0300

    feat: add POST, PUT and DELETE methods

--- api.rest/src/main/java/com/api/rest/controller/PersonController.java
+++ api.rest/src/main/java/com/api/rest/controller/PersonController.java
@@ -4,12 +4,10 @@ import com.api.rest.model.Person;
 import com.api.rest.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
@@ -25,5 +23,36 @@ public class PersonController {
         )
     public Person findById(@PathVariable(value = "id") String id) throws Exception {
         return personServices.findById(id);
+    }
+
+    @RequestMapping(value = "/{id}")
+    public void delete(@PathVariable(value = "id") String id){
+        personServices.delete(id);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+        )
+    public Person create(@RequestBody Person person) {
+        return personServices.create(person);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+        )
+    public Person update(@RequestBody Person person) {
+        return personServices.update(person);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll() {
+        return personServices.findAll();
     }
 }
--- api.rest/src/main/java/com/api/rest/services/PersonServices.java
+++ api.rest/src/main/java/com/api/rest/services/PersonServices.java
@@ -3,6 +3,8 @@ package com.api.rest.services;
 import com.api.rest.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -11,6 +13,41 @@ public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public Person create(Person person) {
+        logger.info("Creating one person");
+        return person;
+    }
+    public Person update(Person person) {
+        logger.info("Updating one person");
+        return person;
+    }
+
+    public void delete(String id) {
+        logger.info("Deleting one person");
+    }
+
+    public List<Person> findAll() {
+
+        logger.info("Finding all person!");
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFistName("Person name " + i);
+        person.setLastName("Last name " + i);
+        person.setAddress("Some address in Brasil " + i);
+        person.setGender(i % 2 == 0 ? "Male" : "Female");
+        return person;
+    }
+
 
     public Person findById(String id) {
 
