fe88617a76c33842967a3e55b444138ae76cc0db
maryson011 <maryson011@gmail.com>
2024-10-24 10:59:17 -0300

    feat: add content negotiation introduction

--- api.rest/src/main/java/com/api/rest/controller/PersonController.java
+++ api.rest/src/main/java/com/api/rest/controller/PersonController.java
@@ -20,37 +20,37 @@ public class PersonController {
 
 
     @PostMapping(
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
         )
     public PersonVO create(@RequestBody PersonVO person) {
         return personServices.create(person);
     }
 
     @PostMapping(value = "/v2",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
         )
     public PersonVOV2 createV2(@RequestBody PersonVOV2 person) {
         return personServices.createV2(person);
     }
 
     @PutMapping(
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
         )
     public PersonVO update(@RequestBody PersonVO person) {
         return personServices.update(person);
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public List<PersonVO> findAll() {
         return personServices.findAll();
     }
 
     @GetMapping(
             value = "/{id}",
-            produces = MediaType.APPLICATION_JSON_VALUE
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
     )
     public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception {
         return personServices.findById(id);
